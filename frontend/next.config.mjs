8a6be798db53f7f855809d862028525cdf65885e
Muhammad Aayan khan <159952659+Aayankhan07@users.noreply.github.com>
2026-07-15 02:33:36 +0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
-      },
-    ];
-  },
+  ...(process.env.NODE_ENV === 'production' 
+    ? {
+        output: 'export',
+        images: {
+          unoptimized: true
+        }
+      } 
+    : {
+        async rewrites() {
+          return [
+            {
+              source: '/api/:path*',
+              destination: 'http://127.0.0.1:8000/api/:path*',
+            },
+          ];
+        },
+      }
+  )
 };
 
 export default nextConfig;
